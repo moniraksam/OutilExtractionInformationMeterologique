9d254e2773fd42e41f399ed308a81719b4bd2f93
awkwardcatlover <shhsammy@gmail.com>
2025-11-09 16:20:49 +0100

    Progression Script Ajout Verification Argument

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 
+#Cas dont la ville est vide (argument positionnel 1 vide)
+
+if [ -z "$1" ]; then
+	echo "Usage Invalide."
+	echo "Dans cette version il faut spécifié une ville au 1er argument"
+	echo "Usage : $0 'nomDeVille'"
+	exit 1
+fi
+
 ville=$1
